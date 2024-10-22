d466d756b516686e3edd932d289c04ef8de71785
Khim070 <109895928+Khim070@users.noreply.github.com>
2024-10-22 22:46:55 +0700

    Initial commit

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI1.Model;
+
+namespace WebAPI1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        // Create => POST
+        // Read => GET
+        // Update => PUT
+        // Delete => DELETE
+
+        // In memory storafe for simplicity
+        private static readonly List<Item> _items = [];
+
+        // GET api/tasks
+        [HttpGet]
+        public ActionResult<IEnumerable<Item>> Get()
+        {
+            return Ok(_items);
+        }
+
+        // GET api/tasks/1
+        [HttpGet("{id}")]
+        public ActionResult<Item> Get(int id)
+        {
+            var item = _items.FirstOrDefault(x => x.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        // POST api/tasks
+        [HttpPost]
+        public ActionResult Post([FromBody] Item item)
+        {
+            _items.Add(item);
+            return CreatedAtAction(nameof(Get), new { id = item.Id }, item);
+        }
+
+        // PUT api/tasks/1
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Item item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            var itemToUpdate = _items.FirstOrDefault(x => x.Id == id);
+            if (itemToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            itemToUpdate.Title = item.Title;
+            itemToUpdate.Description = item.Description;
+            itemToUpdate.IsCompleted = item.IsCompleted;
+
+            return NoContent();
+        }
+
+        // DELETE api/tasks/1
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var itemToDelete = _items.FirstOrDefault(x =>x.Id == id);
+            if(itemToDelete == null)
+            {
+                return NotFound();
+            }
+            _items.Remove(itemToDelete);
+            return NoContent();
+        }
+    }
+}
---
+++ Model/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI1.Model
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted {  get; set; }
+    }
+}
